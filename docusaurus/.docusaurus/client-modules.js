e2cb05d8bb8c0255f30daac7a8bda0355b4738bf
Reis <info@stablecoinstack.org>
2026-03-20 18:14:05 +0200

    Added the docusaurus folder to the .gitignore

--- docusaurus/.docusaurus/client-modules.js
+++
@@ -1,7 +0,0 @@
-export default [
-  require("/home/blackshark/data/fabric-payment-standards/website/docusaurus/.docusaurus/docusaurus-plugin-css-cascade-layers/default/layers.css"),
-  require("/home/blackshark/data/fabric-payment-standards/website/docusaurus/node_modules/infima/dist/css/default/default.css"),
-  require("/home/blackshark/data/fabric-payment-standards/website/docusaurus/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/home/blackshark/data/fabric-payment-standards/website/docusaurus/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/home/blackshark/data/fabric-payment-standards/website/docusaurus/src/css/custom.css"),
-];
--- docusaurus/future_work_and_fixes.md
+++
@@ -1,9 +0,0 @@
-
-# Fixes
-1. The stack is not fully comformant. It must be
-
-2. We are sending the merchant's generated order reference to the contract. That is incorrect, we should send the glema Id instead.
-
-3. Redefinition an repurpose of the `ref` field| Currently holds two values that are not directly related. That design choice has no real benefit.| Each value must be placed in their own field.| That change does not impact the payments or the architecture in general, however, it impacts the implement and reconciliation logic.
-
-4. 
